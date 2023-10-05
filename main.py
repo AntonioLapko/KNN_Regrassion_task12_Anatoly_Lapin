7faa63122b1e31ba8b004397f4c56dd9615006fd
Anatoly <constructor-08@yandex.ru>
2023-10-06 00:35:41 +0400

    1

--- main.py
+++ main.py
@@ -1,63 +1,72 @@
+from sklearn import datasets
 import pandas as pd
 import numpy as np
-from catboost import CatBoostClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, roc_curve
+from mlxtend.plotting import plot_decision_regions
 
-# Генерируем тестовые данные и сохраняем их в файл test.csv
+dataset_diabetes = pd.read_csv('diabetes.csv')
 
-laboratory_work_list = []
-laboratory_work = ("неудовлетворительно", "удовлетворительно", "хорошо", "отлично")
-grant = np.random.randint(0, 100, (2500, 6))
-student_scores = pd.DataFrame (grant, columns = ['subject1', 'subject2', 'subject3', 'subject4', 'subject5', 'subject6'])
-#for i in range (2000): laboratory_work_list.insert (i, np.random.choice(laboratory_work))
-student_scores_avg = pd.DataFrame(student_scores.mean(axis=1))
-a = student_scores_avg.to_numpy()
-for i in range (2500):
-    if a[i] >= 70: laboratory_work_list.insert (i, laboratory_work[3])
-    if a[i] < 70 and a[i] >= 50: laboratory_work_list.insert (i, laboratory_work[2])
-    if a[i] < 50 and a[i] >= 30: laboratory_work_list.insert (i, laboratory_work [1])
-    if a[i] < 30: laboratory_work_list.insert (i, laboratory_work [0])
-student_scores['laboratory_work'] = laboratory_work_list
-student_scores.to_csv('test.csv', index=False)
+#Приведение к нижнему регистру
+dataset_diabetes.columns = [col.lower() for col in dataset_diabetes.columns]
 
-# Загружаем данные из файла test.csv
-student_scores = pd.read_csv('test.csv')
+print(dataset_diabetes.head(), "\n")
+print(dataset_diabetes.info(), "\n")
 
-# Преобразуем категориальные признаки в числовые
-cat_features = ['subject1', 'subject3', 'subject5']
-for feature in cat_features:
-    student_scores[feature] = student_scores[feature].astype('category')
+rows, cols = dataset_diabetes.shape
+print(f"В исходном датасете {rows} строк", "\n")
+print(f"В исходном датасете {cols} столбцов", "\n")
 
-# Разделяем данные на обучающий и тестовый наборы
-X = student_scores.drop('laboratory_work', axis=1)
-y = student_scores['laboratory_work']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#Проведите EDA(Exploratory Data Analysis)
+print(f"Доля пропусков в исходном датасете")
+print(dataset_diabetes.isna().mean().sort_values(), "\n")
 
-# Обучаем модель CatBoostClassifier
-model = CatBoostClassifier(iterations=300, depth=4, learning_rate=0.1, cat_features=cat_features)
-model.fit(X_train, y_train)
+print(f"Максимальные значения в исходном датасете")
+print(dataset_diabetes.max(), "\n")
 
-# Делаем предсказания на тестовом наборе
-y_pred = model.predict(X_test)
+print(f"Минимальные значения в исходном датасете")
+print(dataset_diabetes.min(), "\n")
 
-# Оцениваем точность модели
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Точность модели: {accuracy}")
+print(f"Средние значения в исходном датасете")
+print(dataset_diabetes.mean(), "\n")
 
-#Сохраняем модель в файл
-model.save_model('student_score.cbm')
+print(f"Медиана исходного датасета")
+print(dataset_diabetes.median(), "\n")
 
-# Функция для прогноза оценки итоговой лабораторной работы
+print(f"Дисперсия")
+print(dataset_diabetes.var(), "\n")
 
-def forecast_score (sub1, sub2, sub3, sub4, sub5, sub6):
-    model.load_model('student_score.cbm')
-    new_students_score = pd.DataFrame({'subject1': [sub1], 'subject2': [sub2], 'subject3': [sub3], 'subject4': [sub4], 'subject5': [sub5], 'subject6': [sub6]})
-    new_students_score = new_students_score.astype('int')
-    forecast = model.predict(new_students_score)[0]
-    return print(f"Прогноз итоговой оценки: {forecast}")
+print(f"Среднеквадратическое отклонение")
+print(dataset_diabetes.std(), "\n")
 
-#Необходимо указать оценки студента (от 0 до 99) по 6 предметам
-forecast_score (50, 60, 70, 40, 50, 60)
+# Создание списка значений k, которые оцениваем
+k_values = [num for num in range(5, 26, 5)]
+print(k_values)
 
+# Инициализация списка для сохранения средних оценок точности для каждого значения k
+mean_scores = []
+
+X = dataset_diabetes.drop('outcome', axis=1)
+y = dataset_diabetes['outcome']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state=1234)
+
+
+# Перебор значений k
+for k in k_values:
+    # Создание модели k-ближайших соседей
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+
+    # Вычисление средней оценки точности с использованием перекрестной проверки
+    scores = cross_val_score(knn, X_train, y_train, cv=5, scoring='accuracy')  # cv=5 означает 5-кратную перекрестную проверку
+    mean_scores.append(np.mean(scores))
+
+
+# Вывод результатов
+for k, score in zip(k_values, mean_scores):
+    print (f"k ={k} Mean Accuracy: {score}")
 
